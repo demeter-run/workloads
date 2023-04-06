fa3251f98b31f6be90f5ff7fd7b306374aee9bc6
Alejandro Avagnina <alejandro@accelrobotics.com>
2023-04-06 12:04:51 -0300

    fix: auto-pause wks and config mount (#9)

--- operator/src/workspaces/uptime.ts
+++ operator/src/workspaces/uptime.ts
@@ -3,13 +3,12 @@ import { loadProjectInstances } from '../shared';
 import { API_GROUP, API_VERSION, PLURAL } from './constants';
 import { patchResource } from './handlers';
 
-const EXPIRE_VALUE = process.env.EXPIRE_WORKSPACE_S ? Number(process.env.EXPIRE_WORKSPACE_S) * 1000 : 5 * 60 * 1000;
-const PAUSE_EXPIRED_WORKSPACES = process.env.PAUSE_EXPIRED_WORKSPACES || 'false';
+const EXPIRE_VALUE = process.env.EXPIRE_WORKSPACE_S ? Number(process.env.EXPIRE_WORKSPACE_S) * 1000 : 1800;
+const PAUSE_EXPIRED_WORKSPACES = process.env.PAUSE_EXPIRED_WORKSPACES || 'true';
 
 
 function checkStatus(item: CustomResource<Workspace.Spec, Workspace.Status>, now: number, expire: number): 'active' | 'expired' | 'unknown' {
     if (!item.status.lastSeen) return 'unknown';
-
     // check instant time is fresh. else return active, we don't have data
     if (now - item.status.lastUpdated > 20 * 1000) {
         return 'active';
@@ -18,24 +17,21 @@ function checkStatus(item: CustomResource<Workspace.Spec, Workspace.Status>, now
     if (item.status.lastSeen === 0) return 'unknown';
 
     if (item.status.lastSeen * 1000 < now - expire) return 'expired';
-
     return 'active';
 
 }
 
 function checkUnknown(item: CustomResource<Workspace.Spec, Workspace.Status>, now: number, expire: number): 'active' | 'expired' {
     if (!item.status.startTime) return 'active';
-    if (now - item.status.startTime > 20 * 1000) return 'active';
-
-    if (item.status.startTime > expire) return 'expired';
-
+    if (now - item.status.lastUpdated > 20 * 1000) return 'active';
+    if (now - item.status.startTime > expire) return 'expired';
     return 'active';
 }
 
 export function checkWorkspaceActiveStatus(
     item: CustomResource<Workspace.Spec, Workspace.Status>,
     now: number,
-    expire = EXPIRE_VALUE,
+    expire: number,
 ): 'active' | 'expired' {
     let status = checkStatus(item, now, expire);
     if (status === 'unknown') {
