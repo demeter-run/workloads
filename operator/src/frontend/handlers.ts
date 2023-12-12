8a65323dc155178a8b8ee41e14caf2bfa180f575
Alejandro Avagnina <avatxus@gmail.com>
2023-12-12 18:14:23 -0300

    chore: update image pull policy to Always

--- operator/src/frontend/handlers.ts
+++ operator/src/frontend/handlers.ts
@@ -230,7 +230,7 @@ function containers(
             name: 'main',
             resources: getResourcesFromComputeClass(spec.computeClass),
             image: spec.image,
-            imagePullPolicy: 'IfNotPresent',
+            imagePullPolicy: 'Always',
             volumeMounts,
             args,
             command,
